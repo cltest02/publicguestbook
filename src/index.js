84d9d79f87b80b1462405a47bcc347390e421bf2
Codespaces <codespaces@github.com>
2021-04-19 08:28:03 +0000

    Codespaces

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import App from "./components/App";
-import React from "react";
-import ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
